eaaa59909bad079370fb6a25846668e001bca6f9
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-21 15:26:32 +0900

    feat(g2): export Worker admission contract

--- src/g2/runtime/index.js
+++ src/g2/runtime/index.js
@@ -4,5 +4,6 @@ module.exports = {
   workEvidence: require('./work-evidence'),
   workerState: require('./worker-state'),
   workerRuntime: require('./worker-runtime'),
+  workerAdmission: require('./worker-admission'),
   experiment: require('./experiment')
 };
