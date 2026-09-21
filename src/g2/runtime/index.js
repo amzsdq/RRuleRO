e89065f3eec661e21416af2eae15c6740e5d1ec1
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-21 13:47:54 +0900

    Add G2 worker runtime/utilization: src/g2/runtime/index.js

---
+++ src/g2/runtime/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  workEvidence: require('./work-evidence'),
+  workerState: require('./worker-state'),
+  workerRuntime: require('./worker-runtime'),
+  experiment: require('./experiment')
+};
