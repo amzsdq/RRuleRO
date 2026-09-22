2541feb34a14b3c75f082e707b93198f7da937dd
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-22 13:38:49 +0900

    feat(product): export project compiler

--- src/g2/product/index.js
+++ src/g2/product/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   intake: require('./intake'),
   plan: require('./plan'),
+  projectCompiler: require('./project-compiler'),
   blocker: require('./blocker'),
   reporting: require('./reporting'),
   recursive: require('./recursive')
