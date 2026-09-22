9e92b796604559b6be92d2bfc02e56698b2f645b
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-22 12:47:37 +0900

    feat(g2): export liveness recovery runtime

--- src/g2/runtime/index.js
+++ src/g2/runtime/index.js
@@ -5,5 +5,6 @@ module.exports = {
   workerState: require('./worker-state'),
   workerRuntime: require('./worker-runtime'),
   workerAdmission: require('./worker-admission'),
+  livenessRecovery: require('./liveness-recovery'),
   experiment: require('./experiment')
 };
