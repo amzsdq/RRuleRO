3f8c89f2d888fd7c98ef33414c9af3990a2e1b70
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-22 13:37:58 +0900

    feat(product): export plan lifecycle

--- src/g2/product/index.js
+++ src/g2/product/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   intake: require('./intake'),
+  plan: require('./plan'),
   blocker: require('./blocker'),
   reporting: require('./reporting'),
   recursive: require('./recursive')
