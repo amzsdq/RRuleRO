c2c9543ff2c73a19a3f9a6f0d6a87d54c69840c2
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-22 17:20:24 +0900

    feat(distribution): add unified G2 product entrypoint

---
+++ src/g2/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = Object.freeze({
+  architecture: require('./architecture'),
+  core: require('./core'),
+  scheduler: require('./scheduler'),
+  runtime: require('./runtime'),
+  foreman: require('./foreman'),
+  adapters: require('./adapters'),
+  product: require('./product')
+});
