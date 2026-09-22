e363969bfd9b7d986da39712a57bacbd44afc8ef
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-22 17:34:38 +0900

    feat(scheduler): add host wake adapter bridge (#57)
    * feat(scheduler): add host callback wake adapter
    
    * feat(scheduler): export host wake adapter
    
    * test(scheduler): cover host wake adapter

--- src/g2/scheduler/index.js
+++ src/g2/scheduler/index.js
@@ -4,5 +4,6 @@ module.exports = {
   continuation: require('./continuation'),
   fence: require('./fence'),
   adapterContract: require('./adapter-contract'),
+  toolAdapter: require('./tool-adapter'),
   outcome: require('./outcome')
 };
